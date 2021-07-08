e6e5ff3921d34fd9198a0150025a218315454360
djdembeck <dembeck.david@protonmail.com>
2021-07-08 00:33:26 -0500

    Better docu; bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='m4b-merge',
-    version='0.3.3',
+    version='0.3.4',
     url='https://github.com/djdembeck/m4b-merge',
     description=(
                 "A tool to standardize audiobook files"
