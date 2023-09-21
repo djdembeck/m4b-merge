36c632c9ad03d698f3f740dac65de23c0b158080
Rory McNamara <git@rorym.cnamara.com>
2023-09-21 18:19:48 +0100

    If junk_dir is not set, do not perform post-process move

--- src/m4b_merge/m4b_helper.py
+++ src/m4b_merge/m4b_helper.py
@@ -117,11 +117,12 @@ class M4bMerge:
             exist_ok=True
         )
 
-        # Folder to move original input into
-        Path(config.junk_dir).mkdir(
-            parents=True,
-            exist_ok=True
-        )
+        if config.junk_dir:
+            # Folder to move original input into
+            Path(config.junk_dir).mkdir(
+                parents=True,
+                exist_ok=True
+            )
 
         # Array for argument use
         # main metadata args
@@ -485,6 +486,7 @@ class M4bMerge:
         subprocess.call(args, shell=False)
 
     def move_completed_input(self):
+        if not config.junk_dir: return
         # Cleanup cover file
         if self.cover_path:
             os.remove(self.cover_path)
